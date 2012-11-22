40be63e7bca9c3cd8b378f007f6cac261ebf5fc8
Aileen Pohl <aileen.pohl@student.hpi.uni-potsdam.de>
2012-11-22 17:31:53 +0100

    changed the hello world for raphael to look the same like d3js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,40 +1,43 @@
 $(function() {
 
 	var i = 0;
-	var paper = Raphael('draw', 30, 0, 3000, 3000);
-	var c = paper.circle(50, 60, 30);
+	var paper = Raphael('draw', 30, 0, 0, 0);
+	
+	var line = paper.path("M100,60L320,60");
+		line.attr({
+			opacity: 0.4,
+			stroke: "black"
+		})
+	
+	var c = paper.circle(50, 60, 50);
 	c.attr({
-		fill: 'red',
+		fill: 'steelblue',
 		cursor: 'pointer',
 		opacity: 0.5,
 		stroke: 'white'
 	});	
 	c.node.id = 'red_circle';
 	
-	var b = paper.circle(350, 60, 40);
+	var b = paper.circle(350, 60, 30);
 	b.attr({
-		fill: 'blue',
+		fill: 'grey',
 		opacity: 0.5,
 		stroke: 'white'
 	});
 	
-	var x = paper.circle(300, 100, 20);
-	x.attr({
-		fill: 'yellow',
-		opacity: 0.5,
-		stroke: 'white'
-	});
 	
-	var t = paper.text(50, 10, "Raphaël");
 	
-	var s = $('#red_circle').click(function() {
-		i += 1;
-		if ((i % 2) == 1){
-			b.animate({r: 10, fill: 'green'}, 1000, "backIn");
-		}
-		else{
-			b.animate({r: 40, fill: 'blue'}, 5000, "elastic");
-		}
-	});	
+	
+	var hoverIn = function() {
+        b.attr({"fill": "green"});
+		line.attr({opacity: 1})
+    };
+	
+	  var hoverOut = function() {
+        b.attr({"fill": "grey"});  
+		line.attr({opacity: 0.5})		
+    }
+	
+	  c.hover(hoverIn, hoverOut, c, c);
 	
 });
