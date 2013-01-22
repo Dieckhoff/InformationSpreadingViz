eaa217392adc13cd8b08ff7de7a8cc85defb500c
Julien Bergner <julien.bergner@student.hpi.uni-potsdam.de>
2013-01-22 16:00:16 +0100

    Fetch post details from server and DB

---
+++ src/de/hpi/InformationSpreading/OutputJsonContainer.java
@@ -0,0 +1,25 @@
+package de.hpi.InformationSpreading;
+
+import java.util.List;
+
+public class OutputJsonContainer {
+	
+	private List<Post> posts;
+	
+	public OutputJsonContainer() {
+		super();
+
+	}	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
+	
+	
+
+}
